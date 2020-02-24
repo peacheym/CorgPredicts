27da266a44e17cebfa0959ef059c0e58ad07c3bb
Matt Peachey <matthew_peachey@hotmail.com>
2020-02-24 18:52:16 -0400

    Use the EJS templating engine for dynamic webpages

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 var express = require("express")
 var app = express();
-var path = __dirname + '/views/';
+var path = __dirname + '/public/';
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
 
 app.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+  res.render('index');
 });
 
+
 app.use("*",function(req,res){
-  res.sendFile(path + "404.html");
+  res.render('404');
 });
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
-
